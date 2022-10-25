e3294471e332c37f0c13295d2520df208d7ab54f
CRIPXU <cristofer669galo@gmail.com>
2022-10-25 04:54:29 -0400

    Quinto commit

---
+++ android/app/src/main/kotlin/com/example/animate_radial_menu/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.animate_radial_menu
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'animated_radial_menu.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: new RadialMenuExample(),
+    );
+  }
+}
+
+
